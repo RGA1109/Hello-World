c4cca75e6836d8afa8264e4f0214411878bb9129
Gregg <38334993+RGA1109@users.noreply.github.com>
2019-04-01 06:45:54 -0500

    Add files via upload

--- src/main/java/com/example/fish/PlayerMovementView.java
+++ src/main/java/com/example/fish/PlayerMovementView.java
@@ -8,13 +8,20 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.view.MenuInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
 
 public class PlayerMovementView extends View {
     private Bitmap backgroundImage;
     private Bitmap player[] = new Bitmap[2];
+    private Bitmap enemy[] = new Bitmap[2];
     private Bitmap Inventory[] = new Bitmap[2];
+    private Bitmap gold[] = new Bitmap[2];
+    private Bitmap brick[] = new Bitmap[2];
+    private Bitmap stick[] = new Bitmap[2];
     private boolean touchplayer = false;
     private boolean touchinventory = false;
     private int playerX = 100;
@@ -22,24 +29,17 @@ public class PlayerMovementView extends View {
     private int InventoryX = 0;
     private int InventoryY = 0;
     private int playerSpeed;
-    private int canvasWidth, canvasHeight;
+    private int canvasWidth = 1, canvasHeight = 1;
     private int scoreBrick;
     private int scoreStick;
     private int scoreGold;
     private int yellowX, yellowY, yellowSpeed = 16;
     private int greenX, greenY, greenSpeed = 20;
     private int redX, redY, redSpeed = 25;
-    private int inventoryX, inventoryY;
+    private int enemyX, enemyY, enemySpeed = 10;
     private Paint scorePaintStick = new Paint();
     private Paint scorePaintBrick = new Paint();
     private Paint scorePaintGold = new Paint();
-    private Paint yellowPaint = new Paint();
-    private Paint greenPaint = new Paint();
-    private Paint redPaint = new Paint();
-
-    public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String TEXT = "text";
-    public static final String SWITCH1 = "sitch1";
 
     public PlayerMovementView(Context context)
     {
@@ -47,23 +47,13 @@ public class PlayerMovementView extends View {
 
         player[0] = BitmapFactory.decodeResource(getResources(), R.drawable.player1);
         player[1] = BitmapFactory.decodeResource(getResources(), R.drawable.player2);
-        Inventory[0] = BitmapFactory.decodeResource(getResources(), R.drawable.fish1);
-        Inventory[1] = BitmapFactory.decodeResource(getResources(), R. drawable.fish2);
-
+        enemy[0] = BitmapFactory.decodeResource(getResources(), R.drawable.knife);
+        gold[0] = BitmapFactory.decodeResource(getResources(), R.drawable.gold);
+        stick[0] = BitmapFactory.decodeResource(getResources(), R.drawable.stick);
+        brick[0] = BitmapFactory.decodeResource(getResources(), R.drawable.brick);
+        Inventory[0] = BitmapFactory.decodeResource(getResources(), R.drawable.backpack);
         backgroundImage = BitmapFactory.decodeResource(getResources(), R.drawable.background);
 
-        //Items image will go here
-        yellowPaint.setColor(Color.YELLOW);
-        yellowPaint.setAntiAlias(false);
-
-        greenPaint.setColor(Color.GREEN);
-        greenPaint.setAntiAlias(false);
-
-        redPaint.setColor(Color.RED);
-        redPaint.setAntiAlias(false);
-
-        // items define attributes
-        //Stick
         scorePaintStick.setColor(Color.BLACK);
         scorePaintStick.setTextSize(70);
         scorePaintStick.setTypeface(Typeface.DEFAULT_BOLD);
@@ -95,11 +85,9 @@ public class PlayerMovementView extends View {
         int maxPlayerX = canvasWidth - player[0].getWidth() * 3;
 
         super.onDraw(canvas);
-
         canvasWidth = canvas.getWidth();
         canvasHeight = canvas.getHeight();
-        canvas.drawBitmap(backgroundImage, 0 , 0, null);
-
+        canvas.drawBitmap(backgroundImage, 0 , 0,  null);
 
 
         // player movement in the x direction
@@ -128,9 +116,11 @@ public class PlayerMovementView extends View {
 
         if(touchinventory)
         {
-            canvas.drawBitmap(Inventory[1], InventoryX, inventoryY, null);
+            canvas.drawBitmap(Inventory[1], InventoryX, InventoryY, null);
 
             touchinventory = false;
+
+
         }
         else
         {
@@ -143,7 +133,7 @@ public class PlayerMovementView extends View {
         if (hitBallChecker(yellowX, yellowY))
         {
             scoreGold = scoreGold + 1;
-            yellowY = - 100;
+            yellowY =- 100;
         }
 
         if (yellowY < 0)
@@ -151,7 +141,7 @@ public class PlayerMovementView extends View {
             yellowY = canvasHeight + 21;
             yellowX = (int) Math.floor(Math.random() * (maxPlayerX - minPlayerX)) + minPlayerX;
         }
-        canvas.drawCircle(yellowX, yellowY, 25, yellowPaint);
+        canvas.drawBitmap(gold[0], yellowX, yellowY,null);
 
         // Movement for the Brick item
         greenY = greenY - greenSpeed;
@@ -159,7 +149,7 @@ public class PlayerMovementView extends View {
         {
             scoreBrick = scoreBrick + 1;
 
-            greenY = - 100;
+            greenY =- 100;
         }
 
         if (greenY < 0)
@@ -167,14 +157,14 @@ public class PlayerMovementView extends View {
             greenY = canvasHeight + 21;
             greenX = (int) Math.floor(Math.random() * (maxPlayerX - minPlayerX)) + minPlayerX;
         }
-        canvas.drawCircle(greenX, greenY, 25, greenPaint);
+        canvas.drawBitmap(brick[0], greenX, greenY, null);
 
         //Movement for the Stick item
         redY = redY - redSpeed;
         if (hitBallChecker(redX, redY))
         {
             scoreStick = scoreStick + 1;
-            redY = - 100;
+            redX =- 100;
         }
 
         if (redY < 0)
@@ -182,7 +172,22 @@ public class PlayerMovementView extends View {
             redY = canvasHeight + 21;
             redX = (int) Math.floor(Math.random() * (maxPlayerX - minPlayerX)) + minPlayerX;
         }
-        canvas.drawCircle(redX, redY, 25, redPaint);
+        canvas.drawBitmap(stick[0], redX, redY,null);
+
+        enemyY = enemyY - enemySpeed;
+        if(hitBallChecker(enemyX, enemyY))
+        {
+            scoreStick = scoreStick - 5;
+            scoreGold = scoreGold - 5;
+            scoreBrick = scoreBrick - 5;
+            enemyY =- 100;
+        }
+        if (enemyY < 0)
+        {
+            enemyY = canvasHeight + 21;
+            enemyX = (int) Math.floor(Math.random() * (maxPlayerX - minPlayerX)) + minPlayerX;
+        }
+        canvas.drawBitmap(enemy[0], enemyX, enemyY, null);
 
         //Scores for the number of items collected while moving on the screen
         canvas.drawText("Stick - " + scoreStick,20,60, scorePaintStick);
@@ -190,6 +195,14 @@ public class PlayerMovementView extends View {
         canvas.drawText("Gold - " + scoreGold,20,180, scorePaintGold);
 
     }
+
+//    public void showPopup(View v){
+//        PopupMenu popup = new PopupMenu(this, v);
+//        MenuInflater inflater = popup.getMenuInflater();
+//        inflater.inflate(R.menu.example_menu, popup.getMenu());
+//        popup.show();
+//
+//    }
 
     //function check to see if the player has touched one of the items on the screen
     public boolean hitBallChecker(int x, int y)
@@ -210,14 +223,15 @@ public class PlayerMovementView extends View {
         if (event.getAction() == MotionEvent.ACTION_DOWN)
         {
             touchplayer = true;
-
             playerSpeed = -22;
-
         }
-        if(event.getAction() == MotionEvent.ACTION_DOWN)
+        if(event.getAction() == MotionEvent.ACTION_HOVER_ENTER)
         {
             touchinventory = true;
-
+            if(event.getAction() == MotionEvent.ACTION_HOVER_EXIT)
+            {
+                touchinventory = false;
+            }
         }
 
         return true;
